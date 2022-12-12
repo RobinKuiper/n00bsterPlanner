b789e636265147721d56a0820ea727d72cd87ac8
Robin Kuiper <robingjkuiper@gmail.com>
2022-12-12 19:51:08 +0100

    Initial Commit

---
+++ bin/doctrine.php
@@ -0,0 +1,15 @@
+<?php
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../config/bootstrap.php';
+
+$entityManager = $app->getContainer()->get(EntityManager::class);
+
+$commands = [];
+
+ConsoleRunner::run(new SingleManagerProvider($entityManager), $commands);
