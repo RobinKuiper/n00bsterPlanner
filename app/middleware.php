da40f1f4a443ec27328e914e83c755947d90237d
Robin Kuiper <robingjkuiper@gmail.com>
2022-12-28 20:27:57 +0100

    Frontend&Backend

--- app/middleware.php
+++ app/middleware.php
@@ -3,14 +3,24 @@
 use App\Application\Middleware\ValidationExceptionMiddleware;
 use Selective\BasePath\BasePathMiddleware;
 use Slim\App;
+use Slim\Exception\HttpNotFoundException;
 
 return function (App $app) {
     $setting = $app->getContainer()->get('settings');
 
     $app->addBodyParsingMiddleware();
+    $app->add(\App\Application\Middleware\CorsMiddleware::class);
     $app->add(ValidationExceptionMiddleware::class);
     $app->addRoutingMiddleware();
     $app->add(BasePathMiddleware::class);
 //    $app->add(ErrorMiddleware::class);
     $app->addErrorMiddleware(true, true, true);
+
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
+
+//    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+//        throw new HttpNotFoundException($request);
+//    });
 };
---
+++ src/Application/Middleware/CorsMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Application\Middleware;
+
+use Exception;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Routing\RouteContext;
+
+class CorsMiddleware extends Middleware
+{
+    /**
+     * @param Request $request
+     * @param RequestHandler $handler
+     * @return ResponseInterface
+     * @throws Exception
+     */
+    public function process(Request $request, RequestHandler $handler): ResponseInterface
+    {
+        $routeContext = RouteContext::fromRequest($request);
+        $routingResults = $routeContext->getRoutingResults();
+        $methods = $routingResults->getAllowedMethods();
+        $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
+        $response = $handler->handle($request);
+
+        $response = $response->withHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+        $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+        $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+        // Optional: Allow Ajax CORS requests with Authorization header
+        $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
+    }
+}
